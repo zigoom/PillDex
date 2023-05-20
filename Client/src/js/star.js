2521dc99c9988752a4bd07fd65491de86b008d89
민형기 <alsgudrl132@gmail.com>
2023-05-20 15:05:37 +0900

    마이페이지 수정 및 파일명 변경

---
+++ Client/src/js/star.js
@@ -0,0 +1,10 @@
+let bookmark = document.querySelector("#star");
+bookmark.addEventListener("click", function () {
+  if (bookmark.classList.contains("fa-regular")) {
+    bookmark.classList.remove("fa-regular");
+    bookmark.classList.add("fa-solid");
+  } else {
+    bookmark.classList.remove("fa-solid");
+    bookmark.classList.add("fa-regular");
+  }
+});
