7dfdb10d1549e89ab5d511385cd1fae7f686647c
민형기 <alsgudrl132@gmail.com>
2023-05-31 10:23:59 +0900

    검색조건

--- Server/loadApi/src/main/java/com/pcwk/ehr/ParsingMain.java
+++ Server/loadApi/src/main/java/com/pcwk/ehr/ParsingMain.java
@@ -1,19 +1,21 @@
 package com.pcwk.ehr;
 
 import java.util.List;
+import java.util.Set;
 
 public class ParsingMain {
+    public static void main(String[] args) {
+        String filePath = "C:\\JAVA0309\\FIVEGUYS\\Server\\loadApi\\sbShapeData.csv";  // 데이터 파일의 경로로 설정해야 합니다.
+        String shape = "원형";  // 원하는 모양 설정
+        String color = "하양";  // 원하는 색상 설정
+        String chart = "정제";  // 원하는 제형 설정
+        String line = "분할선";  // 원하는 분할선 설정
 
-	public static void main(String[] args) {
+        ParserShape parser = new ParserShape();
+        Set<String> matchingItems = parser.parseData(filePath, shape, color, chart, line);
 
-		ParserName parserName = new ParserName();
-		parserName.parseDataDetail("C:\\JAVA0309\\FIVEGUYS\\Server\\loadApi\\sbNameData.csv", "대화이부");
-
-//		ParserShape parserShape = new ParserShape();
-//	    List<String> matchingItems = parserShape.parseDataDetailLine("C:\\JAVA0309\\FIVEGUYS\\Server\\loadApi\\sbShapeData.csv", "없음");
-//	    for (String item : matchingItems) {
-//	        System.out.println(item);
-//	    }
-	}
-
+        for (String item : matchingItems) {
+            System.out.println(item);
+        }
+    }
 }
---
+++ Server/loadApi/src/main/java/com/pcwk/ehr/Test.java
@@ -0,0 +1,60 @@
+package com.pcwk.ehr;
+
+public class Test {
+	String itemName;
+	String enptName; 
+	String itemImage;
+	String chart;
+	String printFront; 
+	String printBack;
+	String drugShape;
+	String colorClass;
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getEnptName() {
+		return enptName;
+	}
+	public void setEnptName(String enptName) {
+		this.enptName = enptName;
+	}
+	public String getItemImage() {
+		return itemImage;
+	}
+	public void setItemImage(String itemImage) {
+		this.itemImage = itemImage;
+	}
+	public String getChart() {
+		return chart;
+	}
+	public void setChart(String chart) {
+		this.chart = chart;
+	}
+	public String getPrintFront() {
+		return printFront;
+	}
+	public void setPrintFront(String printFront) {
+		this.printFront = printFront;
+	}
+	public String getPrintBack() {
+		return printBack;
+	}
+	public void setPrintBack(String printBack) {
+		this.printBack = printBack;
+	}
+	public String getDrugShape() {
+		return drugShape;
+	}
+	public void setDrugShape(String drugShape) {
+		this.drugShape = drugShape;
+	}
+	public String getColorClass() {
+		return colorClass;
+	}
+	public void setColorClass(String colorClass) {
+		this.colorClass = colorClass;
+	} 
+}
