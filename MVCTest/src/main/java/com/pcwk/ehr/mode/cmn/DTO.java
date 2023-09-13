0e5b5ed22d82ff29931186bc55fc8f17525ce4c8
김종훈 <rlawhdgns72@naver.com>
2023-09-13 14:53:44 +0900

    사용하지 않는 파일 삭제

--- MVCTest/src/main/java/com/pcwk/ehr/mode/cmn/DTO.java
+++
@@ -1,5 +0,0 @@
-package com.pcwk.ehr.mode.cmn;
-
-public class DTO {
-
-}
--- MVCTestOld/src/main/java/com/pcwk/ehr/mode/dao/ModeDao.java
+++
@@ -1,8 +0,0 @@
-package com.pcwk.ehr.mode.dao;
-
-import com.pcwk.ehr.mode.cmn.WorkDiv;
-import com.pcwk.ehr.mode.domain.PillVO;
-
-public interface ModeDao extends WorkDiv<PillVO> {
-	
-}
